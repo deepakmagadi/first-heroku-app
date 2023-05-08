52a284c325c52f2becae79d5fba39d485a9f0bb0
Deepak G <AL5507@accionlabs.in>
2023-05-08 20:13:32 +0530

    test heroku

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "You have connected with production server",
+			"message": "You have connected with uat server",
 		})
 	})
 	port := os.Getenv("PORT")
