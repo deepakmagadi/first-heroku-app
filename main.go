8e8e95463530e817ab797d85bb2d3883098fdf51
Deepak G <AL5507@accionlabs.in>
2023-05-08 20:08:18 +0530

    test heroku

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	port := os.Getenv("PORT")
+	r.Run(":" + port) // listen and serve on 0.0.0.0:8080
+}
