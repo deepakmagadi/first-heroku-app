532603c23bbcdc0691db5878b708fa6392262385
Deepak G <AL5507@accionlabs.in>
2023-05-09 19:07:25 +0530

    adding config variables

--- main.go
+++ main.go
@@ -10,9 +10,10 @@ import (
 func main() {
 	godotenv.Load()
 	r := gin.Default()
+	env := os.Getenv("ENV")
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "You have connected with production server",
+			"message": "You have connected with " + env + " server",
 		})
 	})
 	port := os.Getenv("PORT")
