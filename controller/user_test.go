97702acd4ad1eaecfd54b7cda21e84a7a97c67e1
Deepak G <AL5507@accionlabs.in>
2023-05-12 17:41:03 +0530

    add DB schema

--- controller/user_test.go
+++ controller/user_test.go
@@ -3,10 +3,14 @@ package controller
 import (
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
 )
 
 func TestGetUser(t *testing.T) {
-	resp := GetUser()
+	var c *gin.Context
+	var db *gorm.DB
+	resp, _ := GetUser(c, db)
 	assert.Equal(t, resp, "test user")
 }
